7c7dcd455bc5eab8dfeeef7d8e3693c68e6ff7d4
wangleei <aaaccchen@163.com>
2026-03-25 22:57:09 +0800

    Update target.mk

--- target/linux/mediatek/filogic/target.mk
+++ target/linux/mediatek/filogic/target.mk
@@ -8,3 +8,13 @@ KERNELNAME:=Image dtbs
 define Target/Description
 	Build firmware images for MediaTek Filogic ARM based boards.
 endef
+
+# -----------------------
+# 新增 N60-Pro 设备条目
+define Device/netcore_n60_pro
+  DTS := mt7986a-netcore-n60-pro.dts
+  IMAGE_SIZE := 16064k
+  DEVICE_PACKAGES := kmod-leds kmod-button-hotplug
+endef
+
+TARGET_DEVICES += netcore_n60_pro
